c9f907607e1a73af64d1daa039ea5de2cb3936c9
daiyongqiang <daiyongqiang@126.com>
2017-01-23 16:46:50 +0800

    继承BaseActivity

--- app/src/main/java/com/ihope/bestwealth/MainActivity.java
+++ app/src/main/java/com/ihope/bestwealth/MainActivity.java
@@ -1,9 +1,10 @@
 package com.ihope.bestwealth;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.ihope.bestwealth.ui.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/ihope/bestwealth/ui/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.ihope.bestwealth.ui;
+
+import android.app.Activity;
+
+/**
+ * Created by DYQ on 2017/1/23.
+ */
+
+public class BaseActivity extends Activity {
+
+}
